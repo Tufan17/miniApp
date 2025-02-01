a0b42c2670b86510142f079d1641ff31a3d1be48
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2025-02-01 11:34:33 +0300

    fixed

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const authenticate = require("../middleware/authenticate")
 const AuthService = require("../services/AuthServices");
-const {loginValidation,registerValidation} = require("../validations/AuthValidation")
+const {loginValidation,registerValidation,loginNicknameValidation} = require("../validations/AuthValidation")
 const validation = require("../middleware/validate")
 const router = express.Router();
 
 router.post("/login",validation(loginValidation),AuthService.loginUser)
-router.post("/login/nickname",validation(loginValidation),AuthService.loginNicknameUser)
+router.post("/login/nickname",validation(loginNicknameValidation),AuthService.loginNicknameUser)
 router.post("/register",validation(registerValidation),AuthService.registerUser)
 router.post("/logout",authenticate,AuthService.logoutUser)
 
--- src/validations/AuthValidation.js
+++ src/validations/AuthValidation.js
@@ -8,6 +8,13 @@ const registerValidation = Joi.object({
     avatar: Joi.string().optional(),
 });
 
+const loginNicknameValidation = Joi.object({
+    nickname: Joi.string().required(),
+    password: Joi.string().required(),
+    rememberMe: Joi.boolean().optional(),
+    deviceId: Joi.string().optional(),
+});
+
 const loginValidation = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
@@ -15,4 +22,4 @@ const loginValidation = Joi.object({
     deviceId: Joi.string().optional(),
 });
 
-module.exports = { registerValidation, loginValidation };
+module.exports = { registerValidation, loginValidation, loginNicknameValidation };
