b7cb9de5ad42828ae9293127af3619f1b1cd9235
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2025-02-24 21:05:05 +0300

    fixed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/login";
 import { useNavigate } from "react-router-dom";
 import DashboardPage from "./pages/dasboard/index";
 import LoaderComponent from "./components/loader";
+import PrivacyPolicy from "./pages/privacy_policy";
 function App() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
@@ -13,8 +14,10 @@ function App() {
     const token = localStorage.getItem("token");
     const user = localStorage.getItem("user");
     setLoading(false);
-    if (!token && !user) {
+    if (!token && !user && location.pathname !== "/privacy-policy") {
       navigate("/login");
+    }else if(location.pathname === "/privacy-policy"){
+      return;
     }else if(location.pathname === "/"){
       navigate("/dashboard");
     }
@@ -31,6 +34,7 @@ function App() {
           <Route path="/" element={<LoaderComponent />} />
           <Route path="/dashboard/*" element={<DashboardPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
       )}
     </div>
---
+++ admin/src/pages/privacy_policy.jsx
@@ -0,0 +1,75 @@
+const PrivacyPolicy = () => {
+    return (
+        <div className="p-4 container mx-auto space-y-4">
+            <div className="flex justify-center items-center">
+                <h1 className="text-2xl font-bold">Privacy Policy</h1>
+            </div>
+
+            <p className="text-sm text-gray-500">Last Updated: 02.24.2025</p>
+
+            <p className="text-sm text-gray-500">This Privacy Policy explains how personal data of users is collected, used, protected, and processed in connection with the services provided by Word Soup. The application has been developed and operated in accordance with the laws of the Republic of Turkey by Word Soup.</p>
+
+            <h5 className="text-lg font-bold">1. Collected Information</h5>
+
+            <p className="text-sm text-gray-500">The following types of information may be collected when you use the application:</p>
+
+            <p className="text-sm text-gray-500">Personal Information: Name, email address, phone number (if user registration is required).</p>
+
+            <p className="text-sm text-gray-500">Device and Usage Information: IP address, browser type, operating system, device identifiers.</p>
+
+            <p className="text-sm text-gray-500">Game Data: In-game progress, scores, and achievements.</p>
+
+            <p className="text-sm text-gray-500">Cookies and Tracking Technologies: Cookies may be used to enhance user experience.</p>
+
+            <h5 className="text-lg font-bold">2. Use of Information</h5>
+
+            <p className="text-sm text-gray-500">The collected information may be used for the following purposes:</p>
+
+            <p className="text-sm text-gray-500">To ensure and improve the operation of the application,</p>
+
+            <p className="text-sm text-gray-500">To manage user accounts,</p>
+
+            <p className="text-sm text-gray-500">To personalize the gaming experience,</p>
+
+            <p className="text-sm text-gray-500">To detect and prevent security breaches,</p>
+
+            <p className="text-sm text-gray-500">To comply with legal obligations.</p>
+
+            <h5 className="text-lg font-bold">3. Sharing of Information</h5>
+
+            <p className="text-sm text-gray-500">Personal data is not shared with third parties except in the following situations:</p>
+
+            <p className="text-sm text-gray-500">With user consent,</p>
+
+            <p className="text-sm text-gray-500">As required by legal obligations,</p>
+
+            <p className="text-sm text-gray-500">With service providers (e.g., hosting services, analytics providers).</p>
+
+            <h5 className="text-lg font-bold">4. Data Storage and Protection</h5>
+
+            <p className="text-sm text-gray-500">User data is stored with necessary security measures in place. However, no data transmission over the internet can be guaranteed to be 100% secure.</p>
+
+            <h5 className="text-lg font-bold">5. Third-Party Links</h5>
+
+            <p className="text-sm text-gray-500">The application may contain links to third-party websites or services. We are not responsible for the privacy practices of these sites.</p>
+
+            <h5 className="text-lg font-bold">6. Children&apos;s Privacy</h5>
+
+            <p className="text-sm text-gray-500">The application does not knowingly collect personal information from children under the age of 13. If a parent or guardian believes that their child has shared personal information, they can contact us to request the deletion of the data.</p>
+
+            <h5 className="text-lg font-bold">7. User Rights</h5>
+
+            <p className="text-sm text-gray-500">Users have the right to access, correct, delete, and object to the processing of their personal data. To exercise any of these rights, you can contact us.</p>
+
+            <h5 className="text-lg font-bold">8. Changes and Updates</h5>
+
+            <p className="text-sm text-gray-500">This privacy policy may be updated from time to time. It is recommended to regularly check the policy to stay informed about updates.</p>
+
+            <h5 className="text-lg font-bold">9. Contact Information</h5>
+
+            <p className="text-sm text-gray-500">For questions or requests regarding our privacy policy, you can contact us at [Email Address].</p>
+        </div>
+    )
+}
+
+export default PrivacyPolicy;
